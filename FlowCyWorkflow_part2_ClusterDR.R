1499360d1371414add43ccc41dff46107585c128
lb5848 <lbiavat1@jhmi.edu>
2020-08-19 14:31:13 -0400

    DR

---
+++ FlowCyWorkflow_part2_ClusterDR.R
@@ -0,0 +1,89 @@
+rm(list = ls())
+# Load packages
+library(ggrastr)
+library(rstudioapi)
+library(devtools)
+library("flowCore")
+library("flowWorkspace")
+library(cytofCore)
+library(FlowSOM)
+# library(cytofkit)
+library(cluster)
+library(Rtsne)
+library(ggplot2)
+library(dplyr)
+library(flowViz)
+library(scales)
+library(ggthemes)
+library(RColorBrewer)
+library(uwot)
+library(CATALYST)
+library(Rphenograph)
+library(diffcyt)
+library(SummarizedExperiment)
+library(stringr)
+library(ggcyto)
+library(SingleCellExperiment)
+library(scran)
+library(scater)
+library(flowVS)
+library(readxl)
+library(flowStats)
+library(FlowSOMworkshop)
+library(flowAI)
+library(CytoNorm)
+library(PeacoQC)
+library(CytoML)
+options(java.parameters="-Xmx60G")
+library(tidyverse)
+library(data.table)
+# library(scuttle)
+# library(iMUBAC)
+library(ggpubr)
+
+
+
+# Set PrimaryDirectory where this script is located
+dirname(rstudioapi::getActiveDocumentContext()$path)  
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+PrimaryDirectory <- getwd()
+PrimaryDirectory
+# Define workingDirectory
+wdName <- "200819_Working_DirectoryFCS"
+workingDirectory <- paste(PrimaryDirectory, wdName, sep = "/")
+
+setwd(workingDirectory)
+
+# Load workspace and SCEobject
+sce <- readRDS("SCE_part1.rds")
+CATALYST::pbMDS(sce, by = "sample_id", color_by = "condition", features = type_markers(sce), fun = "median", label_by = NULL)
+# Run FlowSOM and ConsensusClusterPlus
+seed <- 123456
+set.seed(seed)
+sce <- cluster(sce, features = "type", xdim = 10, ydim = 10, maxK = 20, 
+               verbose = TRUE, seed = seed)
+delta_area(sce)
+# Run dimensionality reduction
+n_cells <- 3000
+n_events <- min(n_cells(sce))
+if(!(n_cells < n_events))
+  n_cells <- n_events
+
+exaggeration_factor <- 12.0
+eta <- n_cells/exaggeration_factor
+sce <- runDR(sce, dr = "TSNE", cells = n_cells, features = "type", theta = 0.5, max_iter = 1000, 
+             distMethod = "euclidean",
+             PCA = TRUE, eta = eta, exaggeration_factor = 12.0)
+sce <- runDR(sce, dr =  "UMAP", cells = n_cells, features = "type")
+sce <- runDR(sce, dr = "DiffusionMap", cells = n_cells, features = "type", assay = "exprs")
+
+saveRDS(sce, file = "SCE_part2_DR.rds")
+
+# make some plots and check everything is fine
+
+CATALYST::plotDR(sce, dr = "UMAP", color_by = "meta8", facet_by = "condition")
+CATALYST::plotDR(sce, dr = "TSNE", color_by = "meta8", facet_by = "condition")
+CATALYST::plotDR(sce, dr = "DiffusionMap", color_by = "meta8", facet_by = "condition")
+CATALYST::plotDR(sce, dr = "UMAP", color_by = "CD8", facet_by = "condition")
+
